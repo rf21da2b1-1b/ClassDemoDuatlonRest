89093a20bfd7305fb3f5462c8a937a004f812b85
Peter Levinsky <pele@zealand.dk>
2022-11-10 11:29:49 +0100

    Add project files.

---
+++ ClassDemoDuatlonRest/Controllers/DuathletesController.cs
@@ -0,0 +1,160 @@
+﻿using ClassDemoDuatlonLib.model;
+using ClassDemoDuatlonRest.managers;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ClassDemoDuatlonRest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DuathletesController : ControllerBase
+    {
+        /*
+         * opret manager
+         */
+        private readonly DuathletesManager _mgr = new DuathletesManager();
+
+
+        // GET: api/<DuathletesController>
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public IActionResult Get()
+        {
+            List<Duathlete> liste = _mgr.GetAll();
+            return (liste.Count == 0) ? NoContent() : Ok(liste);
+            
+            /* samme som
+             * if (liste.Count == 0)
+             * {
+             *    NoContent();
+             * }
+             * else
+             * {
+             *    Ok(liste);
+             * }
+             */
+        }
+
+        // GET api/<DuathletesController>/5
+        [HttpGet("{bib}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult Get(int bib)
+        {
+            try
+            {
+                return Ok(_mgr.GetByBib(bib));
+            }
+            catch(KeyNotFoundException knfe)
+            {
+                return NotFound(knfe.Message);
+            }
+            
+        }
+
+        // POST api/<DuathletesController>
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult Post([FromBody] Duathlete dua)
+        {
+            try
+            {
+                dua.Validate();
+                Duathlete createdDuathlete = _mgr.Add(dua);
+                return Created("api/Duathletes/" + createdDuathlete.Bib, createdDuathlete);
+            }
+            catch(ArgumentException ae)
+            {
+                return BadRequest(ae.Message);
+            }
+
+        }
+
+        
+
+        /* IKKE EN DEL AF Spørgmål
+        
+        // PUT api/<DuathletesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        */
+
+        // DELETE api/<DuathletesController>/5
+        [HttpDelete("{bib}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult Delete(int bib)
+        {
+            try
+            {
+                Duathlete dua = _mgr.Delete(bib);
+                return Ok(dua);
+            }
+            catch (KeyNotFoundException knfe)
+            {
+                return NotFound(knfe.Message);
+            }
+        }
+
+
+
+
+
+
+        /*
+         * Opgave 7
+         */
+        // POST api/<DuathletesController>
+        [HttpPost]
+        [Route("Age/{age}")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult Post(int age, [FromBody] Duathlete dua)
+        {
+            try
+            {
+                if (age < 1 || 125 < age)
+                {
+                    throw new ArgumentException("age skal være mellem 1-125");
+                }
+
+                Duathlete createdDuathlete = _mgr.Add(age, dua);
+                return Created("api/Duathletes/" + createdDuathlete.Bib, createdDuathlete);
+            }
+            catch (ArgumentException ae)
+            {
+                return BadRequest(ae.Message);
+            }
+
+        }
+
+
+        [HttpGet]
+        [Route("AgeGroup/{ageGroup}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult GetByAge(int ageGroup)
+        {
+            try
+            {
+                Duathlete.CheckAgeGroup(ageGroup);
+                List<Duathlete> liste = _mgr.GetAllByAge(ageGroup);
+                return (liste.Count == 0) ? NoContent() : Ok(liste);
+            }
+            catch(Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+
+            
+        }
+
+    }
+}
---
+++ ClassDemoDuatlonRest/managers/DuathletesManager.cs
@@ -0,0 +1,93 @@
+﻿using ClassDemoDuatlonLib.model;
+
+namespace ClassDemoDuatlonRest.managers
+{
+    public class DuathletesManager
+    {
+        /*
+         * Statisk liste
+         */
+        private readonly static List<Duathlete> _duathletes = new List<Duathlete>()
+        {
+            new Duathlete("Eddy Quick Feet", 4, 100, 7200, 2100),
+            new Duathlete("Heavy Peter", 3, 101, 7520, 2190),
+            new Duathlete("Big Mike", 2, 102, 7350, 2390),
+            new Duathlete("Fat Joey", 4, 103, 8256, 2676),
+            new Duathlete("Magic Thomson", 1, 104, 6475, 2050)
+        };
+
+        // Til auto generering af Bib numre
+        private static int nextBib = 105;
+
+        /*
+         * 4 metoder
+         */
+        public List<Duathlete> GetAll()
+        {
+            return new List<Duathlete>(_duathletes);
+        }
+        public Duathlete GetByBib(int bib)
+        {
+            Duathlete? dua = _duathletes.Find(d => d.Bib == bib);
+            if (dua is null)
+            {
+                throw new KeyNotFoundException();
+            }
+            return dua;
+        }
+        public Duathlete Add(Duathlete dua)
+        {
+            // todo bib autogenereret
+            dua.Bib = nextBib;
+            nextBib++;
+
+            _duathletes.Add(dua);
+            return dua;
+        }
+        public Duathlete Delete(int bib)
+        {
+            Duathlete dua = GetByBib(bib);
+            _duathletes.Remove(dua);
+            return dua;
+        }
+
+
+        /*
+         * Opgave 7
+         */
+        public Duathlete Add(int age, Duathlete dua)
+        {
+            dua.AgeGroup = ConvertAgeToGroup(age);
+            dua.Validate();
+            return Add(dua);
+        }
+
+        public List<Duathlete> GetAllByAge(int group)
+        {
+            return _duathletes.FindAll( d => d.AgeGroup == group);
+        }
+
+        private int ConvertAgeToGroup(int age)
+        {
+            if (age < 25)
+            {
+                return 1;
+            }
+
+            if (age <= 35)
+            {
+                return 2;
+            }
+
+            if (age <= 45)
+            {
+                return 3;
+            }
+
+            return 4;
+            
+
+
+        }
+    }
+}
